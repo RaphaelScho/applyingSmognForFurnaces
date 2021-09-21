90b0f264b1ff13199b3f7d8a319d14fcb1f8ca84
RaphaelScho <signup+github@simplelogin.co>
2021-09-21 10:17:48 +0200

    show possible data_path options in config file

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 data_path = "../data/Arnhem_clean_2003-2004.nc"
 #data_path = "../data/Arnhem_clean_2003-2006.nc"
+#data_path = "../data/dataset_smogn_2003-2012.nc"
+#data_path = "../data/australia_9y.nc"
 resample_path = "/home/user1/Documents/Uni Stuff/2021 SS/inderdisc_project_smogn/out/smogn_resampled.pkl"
